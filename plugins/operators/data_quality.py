9620128873a62303186594ab3eb7cbaff3367716
Abdallah El-Sawy <67811312+abdallah-elsawy@users.noreply.github.com>
2022-03-02 18:54:36 +0200

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -21,17 +21,33 @@ class DataQualityOperator(BaseOperator):
         self.check = dq_checks
         self.redshift_conn_id = redshift_conn_id
 
-
+        
+        
+        
+        
     def execute(self, context):
     	redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+    	for table in self.tables:
+            results = redshift.get_records("SELECT COUNT(*) FROM {}".format(table))
+            count = len(results)
+            count_0 = len(results[0])
+            
+            if count  < 1 or count_0 < 1 :
+                raise ValueError(f"DataQualityOperator implemented")
+                
+            self.log.info('DataQualityOperator passed')        
+        
 
-        for check in self.dq_checks:
-            sql = check.get('check_sql')
-            exp_result = check.get('expected_result')
+#     def execute(self, context):
+#     	redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
-            records = redshift.get_records(sql)[0]
-            num_records = records[0][0]
-            if num_records != exp_result:
-                raise ValueError(f"DataQualityOperator failed")
-            else:
-                self.log.info(f"Data quality on passed")              
+#         for check in self.dq_checks:
+#             sql = check.get('check_sql')
+#             exp_result = check.get('expected_result')
+
+#             records = redshift.get_records(sql)[0]
+#             num_records = records[0][0]
+#             if num_records != exp_result:
+#                 raise ValueError(f"DataQualityOperator failed")
+#             else:
+#                 self.log.info(f"Data quality on passed")              
