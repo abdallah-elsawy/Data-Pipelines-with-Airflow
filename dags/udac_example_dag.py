766d6c6aa40a43d588bb45e25a0dda0f886ad3eb
Abdallah El-Sawy <67811312+abdallah-elsawy@users.noreply.github.com>
2022-03-02 18:28:08 +0200

    Update udac_example_dag.py

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -114,13 +114,16 @@ load_time_dimension_table = LoadDimensionOperator(
     sql_query=SqlQueries.time_table_insert
 )
 
-
-
+    dq_checks=[
+        {'check_sql': "SELECT COUNT(*) FROM users WHERE userid is null", 'expected_result': 0},
+        {'check_sql': "SELECT COUNT(*) FROM songs WHERE songid is null", 'expected_result': 0}
+    ]
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
     dag=dag,
     redshift_conn_id="redshift",
+    dq_checks=dq_checks
     tables=[ "songplays", "songs", "artists",  "time", "users"]
 )
 
